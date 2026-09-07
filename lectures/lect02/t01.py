970698d408ef3e80f0e295ab689ecefa3b1e5fe0
Andrii Krenevych <krenevych@knu.ua>
2026-09-07 09:20:22 +0300

    lect02

---
+++ lectures/lect02/t01.py
@@ -0,0 +1,28 @@
+x = 144
+greeting = "Hello"
+
+y = 144
+# print(f"{greeting=}")
+z = x
+# print(f"{z=}")
+x = 13
+# print(f"{x=}")
+
+t = x + y
+# print(t)
+
+c = 2 + 2 * 2  # 6
+# print(f"{c=}")
+c1 = (2 + 2) * 2  # 8
+# print(f"{c1=}")
+
+
+# print(f"{c=}")
+# print(f"{c1=}")
+
+i = 1
+i += 1
+# print(f"{i=}")
+i *= 2
+print(f"{i=}")
+
