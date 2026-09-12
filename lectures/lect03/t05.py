4c609c9df75d4c097a315714a4f50a89dba953e7
Andrii Krenevych <krenevych@knu.ua>
2026-09-12 09:37:13 +0300

    lect03

---
+++ lectures/lect03/t05.py
@@ -0,0 +1,8 @@
+x=int(input("x = "))
+
+if x >= 0:
+    modul_x = x
+else:
+    modul_x = -x
+
+print(f"|{x}| = {modul_x}")
---
+++ lectures/lect03/t06.py
@@ -0,0 +1,12 @@
+x=int(input("x = "))
+
+# modul_x = x
+# if x < 0:
+#     modul_x = -x
+#
+# print(f"|{x}| = {modul_x}")
+
+if x < 0:
+    x = -x
+
+print(x)
