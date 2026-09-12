5da1c16dc7de3a3fecffc194bfd6b59346abee10
Andrii Krenevych <krenevych@knu.ua>
2026-09-12 10:15:53 +0300

    lect04

---
+++ lectures/lect04/t01.py
@@ -0,0 +1,14 @@
+# написати програму, що виводить на екран тричі Hello, World!
+# upd: треба 10 разів написати
+# upd: треба 100 разів написати
+# upd: треба стільки разів написати Hello, World! скільки користувач введе з клавіатури
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+# і так далі, поки не буде 100 разів
---
+++ lectures/lect04/t02.py
@@ -0,0 +1,9 @@
+
+N = int(input("Кількість 'Hello World!'"))
+
+counter = 0
+while counter < N:
+    print(counter, "Hello World!")
+    counter += 1
+
+pass
