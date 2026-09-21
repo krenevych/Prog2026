65cb9adabd7eb8d0dbd148e80101e7333d389db1
Andrii Krenevych <krenevych@knu.ua>
2026-09-21 08:52:35 +0300

    lect05

---
+++ lectures/lect05/t01.py
@@ -0,0 +1,19 @@
+# перебрати всі числа від 10 до 100
+
+# counter = 10
+# while counter <= 100:
+#     print(counter)
+#     counter = counter + 1
+
+data = [
+    1, 10, 99, 33, 55, "Hello", "World"
+]
+# перебрати ВСІ числа в колекції даних data (порядок не важливий)
+
+# for element in data:
+#     print(element)
+
+for x in data:
+    print(x)
+
+
